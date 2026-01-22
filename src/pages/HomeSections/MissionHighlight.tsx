9cb7d5640fa745fba270b0398d242ecd1728fd49
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2026-01-22 12:37:11 +0200

    refining

--- src/pages/HomeSections/MissionHighlight.tsx
+++ src/pages/HomeSections/MissionHighlight.tsx
@@ -4,13 +4,13 @@ export default function MissionHighlight() {
   const { t } = useTranslation();
 
   return (
-    <section className="bg-gray-100 text-black py-15 md:py-25">
+    <section className="bg-gray-100 text-black py-15">
       <div className="mx-auto w-10/12 md:w-8/12 lg:w-6/12 px-4 py-14 sm:py-20">
-        <h2 className="mt-4 md:text-3xl text-xl  font-extrabold leading-tight sm:text-5xl">
+        <h2 className="md:text-3xl text-xl font-extrabold leading-tight">
           {t("home.mission.title")}
         </h2>
 
-        <p className="my-10   whitespace-pre-line leading-8  font-medium md:text-justify">
+        <p className="my-10 whitespace-pre-line leading-8  font-medium md:text-justify">
           {t("home.mission.body")}
         </p>
       </div>
