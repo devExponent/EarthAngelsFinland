6191f7be73b611a30902075ea7d1e622c1762852
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2026-01-22 13:38:49 +0200

    refining

--- src/pages/AboutUs/AboutTopSection.tsx
+++ src/pages/AboutUs/AboutTopSection.tsx
@@ -8,8 +8,8 @@ export default function AboutTop() {
 
   return (
     <main className="bg-[#f4f1ea]">
-      <section className="mx-auto w-10/12 px-4 md:py-16">
-        <div className="grid items-center gap-10 lg:grid-cols-2 md:gap-14">
+      <section className="mx-auto w-10/12 md:py-16">
+        <div className="flex flex-col gap-10 items-center md:flex-row">
           <div>
             <p className="my-2 text-xl font-black tracking-widest text-orange-500">
               {t("about.kicker")}
