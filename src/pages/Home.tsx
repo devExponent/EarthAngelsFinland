7f7734b685f3d541e131f2e9f368bf43eb73e7f2
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2026-03-19 10:29:38 +0200

    womens day

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import HeroSection from "./HomeSections/HeroSection";
 import { IntroVideoCard } from "./HomeSections/HeroVideoSection";
 import MissionHighlight from "./HomeSections/MissionHighlight";
 import ImpactThreeCards from "./HomeSections/Impact";
-import OpeningEventSection from "./HomeSections/OpeningEvent";
 import WomensDay from "./HomeSections/WomensDay";
 
 export default function Home() {
